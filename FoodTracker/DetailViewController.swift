d654f49a9272889dd555f59bef7249136f367e89
droomcommissaris <Droomcommissaris@MacBook-Pro-van-Droomcommissaris.local>
2015-07-19 18:30:28 +0200

    dataWithJSONObject troubles

---
+++ FoodTracker/DetailViewController.swift
@@ -0,0 +1,39 @@
+//
+//  DetailViewController.swift
+//  FoodTracker
+//
+//  Created by Droomcommissaris on 19/07/15.
+//  Copyright © 2015 droomcommissaris. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var textView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func eatItButtonPressed(sender: UIBarButtonItem) {
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -8,18 +8,122 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate, UISearchResultsUpdating {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    let kAppID = "c0ee395d"
+    let kAppKey = "6e0e4b5f110bc29c80d22d9ed86aefbf"
+    
+    var searchController: UISearchController!
+    
+    var suggestedSearchFoods:[String] = []
+    var filteredSuggestedSrachFoods:[String] = []
+    
+    var scopeButtonTitles = ["Recommended", "Search Results", "Saved"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.searchController = UISearchController(searchResultsController: nil)
+        self.searchController.searchResultsUpdater = self
+        self.searchController.dimsBackgroundDuringPresentation = false
+        self.searchController.hidesNavigationBarDuringPresentation = false
+        self.searchController.searchBar.frame = CGRect(x: self.searchController.searchBar.frame.origin.x, y: self.searchController.searchBar.frame.origin.y, width: self.searchController.searchBar.frame.size.width, height: 44.0)
+        self.tableView.tableHeaderView = self.searchController.searchBar
+        self.searchController.searchBar.scopeButtonTitles = scopeButtonTitles
+        self.searchController.searchBar.delegate = self
+        
+        self.definesPresentationContext = true
+        
+        self.suggestedSearchFoods = ["apple", "bagel", "banana", "beer", "bread", "carrots", "cheddar cheese", "chicken breast", "chili with beans", "chocolate chip cookie", "coffee", "cola", "corn", "egg", "graham cracker", "granola bar", "green beans", "ground beef patty", "hot dog", "ice cream", "jelly doughnut", "ketchup", "milk", "mixed nuts", "mustard", "oatmeal", "orange juice", "peanut butter", "pizza", "pork chop", "potato", "potato chips", "pretzels", "raisins", "ranch salad dressing", "red wine", "rice", "salsa", "shrimp", "spaghetti", "spaghetti sauce", "tuna", "white wine", "yellow cake"]
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    // Mark - UITableViewDataSource
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        if self.searchController.active {
+            return self.filteredSuggestedSrachFoods.count
+        } else {
+            return self.suggestedSearchFoods.count
+        }
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell") as UITableViewCell!
+        
+        var foodName: String
+        
+        if self.searchController.active {
+            foodName = filteredSuggestedSrachFoods[indexPath.row]
+        } else {
+            foodName = suggestedSearchFoods[indexPath.row]
+        }
+        
+        cell.textLabel?.text = foodName
+        cell.accessoryType = UITableViewCellAccessoryType.DisclosureIndicator
+        
+        return cell
+        
+    }
+    
+    // UISearchResultsUpdate Delegate
+    
+    func updateSearchResultsForSearchController(searchController: UISearchController) {
+        
+        let searchString = self.searchController.searchBar.text!
+        let selectedScopeButtonIndex = self.searchController.searchBar.selectedScopeButtonIndex
+        
+        self.filterContentForSearch(searchString, scope: selectedScopeButtonIndex)
+        tableView.reloadData()
+        
+    }
+    
+    func filterContentForSearch (searchText: String, scope: Int) {
+        self.filteredSuggestedSrachFoods = self.suggestedSearchFoods.filter({ (food : String) -> Bool in
+            let foodMatch = food.rangeOfString(searchText)
+            return foodMatch != nil
+        })
+    }
+    
+    // UISearchbarDelegate
+    
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        makeRequest(searchBar.text!)
+    }
+    
+    func makeRequest(searchString: String){
+        
+//        let url = NSURL(string: "https://api.nutritionix.com/v1_1/search/\(searchString)?results=0%3A20&cal_min=0&cal_max=50000&fields=item_name%2Cbrand_name%2Citem_id%2Cbrand_id&appId=\(kAppID)&appKey=\(kAppKey)")
+//        
+//        let task = NSURLSession.sharedSession().dataTaskWithURL(url!) { (data, response, error) -> Void in
+//            
+//            let stringData = NSString(data: data!, encoding: NSUTF8StringEncoding)
+//            
+//            print(stringData)
+//            print(response)
+//        }
+//        task?.resume()
+        
+        var request = NSMutableURLRequest(URL: NSURL(string: "https://api.nutritionix.com/v1_1/search/")!)
+        let session = NSURLSession.sharedSession()
+        request.HTTPMethod = "POST"
+        
+        var params = ["appId": kAppID, "appKey": kAppKey, "fields": ["item_name", "brand_name", "keywords", "usda_fields"], "limit": "50", "query": searchString, "filters": ["exists": ["usda_fields": true]]]
+        
+        var error: NSError?
+        request.HTTPBody = NSJSONSerialization.dataWithJSONObject(params, options: nil, error: &error)
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        
+    }
 }
 
